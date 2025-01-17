e494dc3b7b77289de58454e1701600f458416aac
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-17 15:35:39 -0300

    chore(server): setup 0.0.0.0 as default api host

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -55,6 +55,6 @@ app.register(authenticateWithPassword)
 
 app.register(authRoutes)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('🚀 HTTP Server Running!')
 })
