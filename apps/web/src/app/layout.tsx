17fc5c846d2970d83a27bcda379900972a96f576
Samuel Cabral <samuelcabral.mail@gmail.com>
2025-01-17 10:25:13 -0300

    feat(web): add Shadcn/UI library components

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export const metadata: Metadata = {
   title: 'Courses Manager Web App',
@@ -12,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark">
+      <body
+        className={cn('bg-background font-sans antialiased', inter.variable)}
+      >
+        {children}
+      </body>
     </html>
   )
 }
